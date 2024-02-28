15ceec30b1031ce5fdda205a62f028c1233c7052
August Brandt <aubr@itu.dk>
2024-02-28 13:12:18 +0100

    Create validator

--- String_con/input_validators/validate.py
+++ String_con/input_validators/validate.py
@@ -3,10 +3,10 @@ import sys
 import re
 
 line = sys.stdin.readline()
-if not re.match(r"(0|[1-9][0-9]*) (0|[1-9][0-9]*) (0|[1-9][0-9]*)\n", line):
+if not re.match(r"([A-z1-9][A-z\d]*) ([A-z1-9][A-z\d]*) ([A-z1-9][A-z\d]*) ([A-z1-9][A-z\d]*)\n", line):
     sys.exit(43)
-a, b, c = map(int, line.split())
-if not (0 <= a <= 10000 and 0 <= b <= 10000 and 0 <= c <= 10000):
+a, b, c, d = map(len, line.split())
+if not (a <= 64 and b <= 64 and c <= 64 and d <= 64):
     sys.exit(43)
 if sys.stdin.readline() != "":
     sys.exit(43)
