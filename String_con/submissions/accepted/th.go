e4b6116d667e65bf7c37088a2abad38f31faaeb6
Theis <thph@itu.dk>
2024-02-28 11:10:41 +0100

    Added a go solution to the submission folder

---
+++ String_con/submissions/accepted/th.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	var input string
+	reader := bufio.NewReader(os.Stdin)
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		os.Exit(1)
+	}
+	lst := strings.Split(input, " ")
+
+	fmt.Print(lst[0], lst[1], lst[2], lst[3])
+}
--- String_con/submissions/accepted/th.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python3
-
-a, b, c = input().split()
-print(int(a) + int(b) + int(c))
