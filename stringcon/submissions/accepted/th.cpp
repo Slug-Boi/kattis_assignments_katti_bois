26e57d1d1d06586fa226d613e720809f1d017d1f
Theis <thph@itu.dk>
2024-02-29 11:20:46 +0100

    Changed my go solution to a cpp program instead since the ICPC tools dont have go installed

---
+++ stringcon/submissions/accepted/th.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <cstring>  
+
+int main() {
+
+    std::string inp;
+    std::getline(std::cin, inp);
+
+    inp.erase(remove_if(inp.begin(),inp.end(), isspace),inp.end());
+
+    std::cout << inp;
+
+    return 0;
+}
