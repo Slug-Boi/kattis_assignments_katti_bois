b3336d2220110c3fda3facce9ca855d01b895dfe
Slug-Boi <thph@itu.dk>
2024-04-28 14:47:15 +0200

    Updated the validator and sample input 1

--- segment_trees/input_validators/validate.py
+++ segment_trees/input_validators/validate.py
@@ -3,12 +3,71 @@ import sys
 import re
 
 line = sys.stdin.readline()
-if not re.match(r"([A-z1-9][A-z\d]*) ([A-z1-9][A-z\d]*) ([A-z1-9][A-z\d]*) ([A-z1-9][A-z\d]*)", line):
-    print("Regex failed")
+
+# Check if the first line is in the correct format
+if not re.match(r"^[1-9]\d* [1-9]\d*\n$", line):
     sys.exit(43)
-a, b, c, d = map(len, line.split())
-if not (a <= 64 and b <= 64 and c <= 64 and d <= 64):
+
+## FIRST LINE CHECKS
+
+# Check if first line is 2 items
+inp = line.split()
+if len(inp) != 2:
     sys.exit(43)
-if sys.stdin.readline() != "":
+    
+# Check if N and O are integers
+N, O = inp   
+if not N.isdigit() or not O.isdigit():
     sys.exit(43)
+
+# Check if N and O are within the bounds
+if not 1 <= int(N) <= 10**5:
+    sys.exit(43)
+if not 1 <= int(O) <= 10**6:
+    sys.exit(43)
+
+
+## ARRAY CHECKS
+
+# Check if second line is N integers
+arr = []
+try:
+    arr = list(map(int, sys.stdin.readline().split()))
+except:
+    sys.exit(43)
+
+# Check if the length of the array is N
+if len(arr) != int(N):
+    sys.exit(43)
+
+# Check if the integers are within the bounds
+for i in arr:
+    if not 0 <= i < 10**2:
+        sys.exit(43)
+        
+## QUERY CHECKS
+O_actual = 0
+
+for _ in range(int(O)):
+    line = sys.stdin.readline()
+    # Update the query count
+    O_actual += 1
+    
+    # Check if the query is in the correct format
+    if not re.match(r"^[MSU] \d+ \d+$", line):
+        sys.exit(43)
+    
+    ch, n1, n2 = line.split()
+    # Check if the queries M and S are within the bounds
+    if ch == 'M' or ch == 'S':
+        if not (0 <= int(n1) < int(n2) <= int(N)):
+            sys.exit(43)
+            
+    # Check if the queries U are within the bounds
+    elif ch == 'U':
+        if not (0 <= int(n1) < int(N) and 0 <= int(n2) < 10**2):
+            sys.exit(43)
+    else:
+        sys.exit(43)
+
 sys.exit(42)
---
+++ segment_trees/sol.py
@@ -0,0 +1,103 @@
+# limit for array size  
+N,O = map(int,input().split())
+# Max size of tree 
+# Nodes in the tree are a tuple of (sum, max)
+# Leaf nodes are just a value (sum, max) = (value, value)
+tree = [(0,0)] * (2 * N)
+# function to build the tree  
+
+def build(arr): 
+  
+    # insert leaf nodes in tree  
+    for i in range(n):  
+        tree[n + i] = (arr[i], arr[i])
+      
+    # build the tree by calculating parents  
+    for i in range(n - 1, 0, -1):  
+        tree[i] = (tree[i << 1][0] + tree[i << 1 | 1][0], max(tree[2 * i][1], tree[2 * i + 1][1]))
+        
+# function to update a tree node  
+def updateTreeNode(p, value):  
+      
+    # set value at position p  
+    p = p + n  
+    tree[p] = (value,value) 
+      
+      
+    # move upward and update parents p functions as i here 
+    while p > 1: 
+        # XOR operation gets the other node. So if we update the left node XOR will get the right node and vice versa
+        tree[p >> 1] = (tree[p][0] + tree[p ^ 1][0], max(tree[p][1], tree[p ^ 1][1]))
+        p >>= 1  
+  
+# function to get sum on interval [l, r)  
+def sum_query(l, r):  
+  
+    res = 0
+      
+    # loop to find the sum in the range  
+    l += n
+    r += n
+      
+    while l < r: 
+        
+        # Could use bitwise & but python automagically translates mod 2 operations to bitwise and
+        if (l % 2): 
+            res += tree[l][0] 
+            l += 1
+      
+        if (r % 2): 
+            r -= 1
+            res += tree[r][0]
+              
+        l >>= 1
+        r >>= 1
+      
+    return res
+
+def max_query(l,r):
+    
+    res = 0 
+      
+    # loop to find the sum in the range  
+    l += n
+    r += n 
+      
+    
+    while l < r: 
+        
+        # Could use bitwise & but python automagically translates mod 2 operations to bitwise and
+        if (l % 2): 
+            res = max(res,tree[l][1])  
+            l += 1
+      
+        if (r % 2): 
+            r -= 1 
+            res = max(res,tree[r][1])  
+              
+        l >>= 1
+        r >>= 1
+      
+    return res 
+
+def parser(ch, n1, n2):
+    match ch:
+        case 'M':
+            print(max_query(n1,n2))
+        case 'S':
+            print(sum_query(n1,n2))
+        case 'U':
+            updateTreeNode(n1,n2)
+ 
+           
+a = list(map(int,input().split()))
+
+# n is the size of the input array  
+n = len(a)
+
+# build tree  
+build(a)
+
+for _ in range(O):
+    ch, n1, n2 = input().split()
+    parser(ch, int(n1), int(n2))
