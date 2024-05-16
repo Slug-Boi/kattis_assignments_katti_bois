e967f52b775509e511bae6a8df04058fa216825b
Theis <thph@itu.dk>
2024-05-16 22:36:59 +0200

    rename knapsack to follow kitty naming scheme

--- kattis_solutions/Knapsack/knapsack.java
+++ kattis_solutions/Knapsack/knapsack.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.io.*;
 
-public class Main {
+public class knapsack {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     public static void main(String[] args) throws IOException {
         String line;
