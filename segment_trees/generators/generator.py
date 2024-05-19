272d1f8b48c34805f55bd6be38c0708f9e12d65f
Theis <thph@itu.dk>
2024-05-19 20:59:21 +0200

    Renamed generator and added bash scripts for input output generation

--- segment_trees/generators/generator.py
+++ segment_trees/generators/generator.py
@@ -9,31 +9,35 @@ if len(args) > 1:
     N = int(argv[1])
     O = int(argv[2])
 else:
-    N = 7 # Length of starting array 0 < N <= 100_000
-    O = 3 # Amount of operations 0 < O <= 1_000_000
+    N = 7  # Length of starting array 0 < N <= 100_000
+    O = 3  # Amount of operations 0 < O <= 1_000_000
+
 
 def CreateStartingArray():
     startingArray = []
     for _ in range(N):
-        startingArray.append(str(random.randint(0,99))) # Add numbers between 0 - 99 both inclusive
-    
+        # Add numbers between 0 - 99 both inclusive
+        startingArray.append(str(random.randint(0, 99)))
+
     return startingArray
+
 
 def CreateOperation():
     operationTypes = ["M", "S", "U"]
     operation = []
-    operation.append(random.choice(operationTypes)) # Add the operation
-    
+    operation.append(random.choice(operationTypes))  # Add the operation
+
     # Create update
     if operation[0] == "U":
-        operation.append(str(random.randint(0, N-1))) # Add the index of the update
-        operation.append(str(random.randint(0, 99))) # Add the updated value
-        return operation 
-    
-    # Create Max and Sum 
+        # Add the index of the update
+        operation.append(str(random.randint(0, N-1)))
+        operation.append(str(random.randint(0, 99)))  # Add the updated value
+        return operation
+
+    # Create Max and Sum
     # Add the first index. Since first index is strictly less than the
     # second it has to be N-1 so the other can still be N
-    operation.append(str(random.randint(0, N-1))) 
+    operation.append(str(random.randint(0, N-1)))
     # Add second index. Since this is strictly greater than the first
     # it will be between first index + 1 and N
     operation.append(str(random.randint(int(operation[1])+1, N)))
@@ -51,3 +55,4 @@ if __name__ == "__main__":
     print(" ".join(CreateStartingArray()))
     for _ in range(O):
         print(" ".join(CreateOperation()))
+
