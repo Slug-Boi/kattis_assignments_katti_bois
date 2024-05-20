de839b5f5ca536f96cf94ab3e7e7d358a8612123
August-Brandt <augustbrandt170@gmail.com>
2024-05-20 10:28:27 +0200

    Fix spelling mistake

--- segment_trees/generators/generator.py
+++ segment_trees/generators/generator.py
@@ -6,15 +6,15 @@ args = argv
 if len(args) == 3:
     N = int(argv[1])
     O = int(argv[2])
-    max_quary_size = N
+    max_query_size = N
 elif len(args) == 4:
     N = int(argv[1])
     O = int(argv[2])
-    max_quary_size = int(argv[3])
+    max_query_size = int(argv[3])
 else:
     N = 7  # Length of starting array 0 < N <= 100_000
     O = 3  # Amount of operations 0 < O <= 1_000_000
-    max_quary_size = N
+    max_query_size = N
 
 
 def CreateStartingArray():
@@ -45,8 +45,8 @@ def CreateOperation():
     # Add second index. Since this is strictly greater than the first
     # it will be between first index + 1 and N
     if operation[0] == "M":
-        # Use max_quary_size to bound the range that max quaries are
-        operation.append(str(random.randint(int(operation[1])+1, min(int(operation[1])+1 + max_quary_size, N))))
+        # Use max_query_size to bound the range that max queries are
+        operation.append(str(random.randint(int(operation[1])+1, min(int(operation[1])+1 + max_query_size, N))))
     else:
         operation.append(str(random.randint(int(operation[1])+1, N)))
     return operation
